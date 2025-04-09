a29867adb5954b992c1ec7e749ad0b5adaed4be4
Mathieu Barriere <tech@barriere.me>
2025-04-09 10:45:52 +0200

    Refactor env handling and improve type safety.
    Reorganized environment variable management by introducing `env-helper` and Zod schemas for validation, ensuring consistent and secure env parsing. Removed outdated constants and replaced the website URL logic with a utility function. Updated dependencies and added Zod for schema validation across the project.

--- apps/dashboard/appConstants/index.tsx
+++
@@ -1,5 +0,0 @@
-export const siteEnv = process.env.NEXT_PUBLIC_SITE_ENV;
-
-export const websiteUrl = `${siteEnv === 'local' ? 'http' : 'https'}://${
-  process.env.NEXT_PUBLIC_WEBSITE_URL || process.env.NEXT_PUBLIC_VERCEL_URL
-}`;
---
+++ apps/dashboard/utils/website-domain.ts
@@ -0,0 +1,13 @@
+import { env } from '@/env';
+
+export function getWebsiteDomain() {
+  const {
+    NEXT_PUBLIC_WEBSITE_URL,
+    NEXT_PUBLIC_VERCEL_URL,
+    NEXT_PUBLIC_SITE_ENV,
+  } = env;
+
+  return `${NEXT_PUBLIC_SITE_ENV === 'local' ? 'http' : 'https'}://${
+    NEXT_PUBLIC_WEBSITE_URL || NEXT_PUBLIC_VERCEL_URL
+  }`;
+}
