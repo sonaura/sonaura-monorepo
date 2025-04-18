e178fd34e0fb26bcec7b88a705126c25d9d3968a
Mathieu Barriere <tech@barriere.me>
2025-04-18 12:01:21 +0200

    Add issuer URL validation and update dependencies
    Introduced a function to validate allowed client IDs and redirect URIs for production environments. Updated `@types/node` and other related dependencies in multiple packages for consistency. Adjusted `wrangler.jsonc` configuration to include environment-specific variables and resource bindings.

--- apps/authentication/src/auth/auth-issuer-middleware.ts
+++ apps/authentication/src/auth/auth-issuer-middleware.ts
@@ -8,14 +8,42 @@ import { getUserId } from '@/auth/get-user-id';
 import { MiddlewareHandler } from 'hono';
 import { sendEmailCode } from '@/email/send-email.handler';
 
+type AllowIssuer = (
+  client: { redirectURI: string; clientID: string; audience?: string },
+  req: Request,
+) => Promise<boolean>;
+
+const authorizedRedirectURI = ['sonaura.fr', 'dashboard-*-sonaura.vercel.app'];
+const authorizedClient = ['sonaura-dashboard'];
+
 export const authIssuerMiddleware: MiddlewareHandler<AppType> = async (c) => {
   const storage = CloudflareStorage({
     namespace: c.env.AUTH_KV,
   });
 
+  const allow: AllowIssuer = async ({ redirectURI, clientID }) => {
+    const isProd = c.env.ENVIRONMENT === 'production';
+
+    if (!isProd) {
+      return true;
+    }
+
+    const isAuthorizedClientID = authorizedClient.includes(clientID);
+
+    const clientUrl = new URL(redirectURI);
+    const isAuthorizedRedirectURI = authorizedRedirectURI.some((uriPattern) => {
+      const regexPattern = uriPattern.replace(/\*/g, '.*');
+      const regex = new RegExp(`^${regexPattern}$`);
+      return regex.test(clientUrl.hostname);
+    });
+
+    return isAuthorizedClientID && isAuthorizedRedirectURI;
+  };
+
   const authIssuer = issuer({
     subjects,
     storage,
+    allow,
     providers: {
       code: CodeProvider(
         CodeUI({
